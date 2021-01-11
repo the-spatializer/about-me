8e2a1ca5be88a058b2114cdbfdb421264c94f103
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2021-01-11 14:21:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,20 +10,10 @@ $(document).ready(function(){
     });
     // transition bottom
     // printing top
-    var ua = navigator.userAgent.toLowerCase();
-    var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
-    $("#btn-prnt").click(function(e){
+    $("#btn-prnt").click(function(){
         $("#card").hide();
         $("#details").hide();
-        e.preventDefault();
-        if (isAndroid) {
-            var gadget = new cloudprint.Gadget();
-            gadget.setPrintDocument("url", $('title').html(), window.location.href, "utf-8");
-            gadget.openPrintDialog();
-        }
-        else {
-            window.print();
-        }
+        window.print();
         $("#details").show();
     });
     //printing bottom
