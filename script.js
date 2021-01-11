b532121c7ea870f8736064434b5dba03c3106985
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2021-01-11 11:45:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ $(document).ready(function(){
         $("#card").fadeOut("fast");
         $("#details").fadeIn("fast");
     });
-    $("#back").click(function(){
+    $("#btn-back").click(function(){
         $("#details").fadeOut("fast");
         $("#card").fadeIn("fast");
     });
-    // transition top
+    // transition bottom
+    //button right top
+    w = screen.width;
+    if (w <= 768) {
+        $("#btn-back, #btn-prnt").addClass("btn-right");
+    };
+    //button bottom
+    // printing top
+    $("#btn-prnt").click(function(){
+        $("#card").hide();
+        $("#details").hide();
+        window.print();
+        $("#details").show();
+    });
+    //printing bottom
 });
