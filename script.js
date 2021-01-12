f50c3cf0466ad6e8049ac444511e0a72779e32ce
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2021-01-12 08:25:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,5 +16,23 @@ $(document).ready(function(){
         window.print();
         $("#details").show();
     });
-    //printing bottom
+    $(document).keydown(function(event) {
+        if (event.ctrlKey== true && (event.which == "80")) {
+            alert("Sorry, you're not allowed to print using ctrl+p. Please use the print button instead.");
+            location.reload();
+        };
+    });
+    // printing bottom
+    // disable selection top
+    function disableSelection(target){
+        if (typeof target.onselectstart!='undefined') //IE route
+            target.onselectstart=function(){return false}
+        else if (typeof target.style.MozUserSelect!='undefined') //Firefox route
+            target.style.MozUserSelect='none'
+        else //All other route (ie: Opera)
+            target.onmousedown=function(){return false}
+        target.style.cursor = 'default'
+    };
+    disableSelection(document.body)
+    // disable selection bottom
 });
